9f12e83f8fef4588566a3f3627c839b3b7d219ea
邢迎坤 <956196155@qq.com>
2023-06-26 16:06:19 +0800

    init something

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<script setup></script>
+
+<template>
+  <div>
+    <div class="relative">
+      <div class="big-round-1"></div>
+      <div class="big-round-2"></div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.relative {
+  position: relative;
+}
+.big-round-1 {
+  width: 60%;
+  height: 400px;
+  border-bottom-right-radius: 100%;
+  background: pink;
+}
+.big-round-2 {
+  width: 50%;
+  height: 300px;
+  border-bottom-left-radius: 100%;
+  background: skyblue;
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+</style>
